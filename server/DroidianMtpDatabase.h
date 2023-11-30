2f944571ec239db380e69e5e12fb57229fb21745
Bardia Moshiri <fakeshell@bardia.tech>
2023-11-30 13:34:18 -0500

    treewide: drop greeter and dbus, adapt to droidian

--- server/DroidianMtpDatabase.h
+++ server/DroidianMtpDatabase.h
@@ -56,7 +56,7 @@ using namespace boost::filesystem;
 
 namespace android
 {
-class UbuntuMtpDatabase : public android::MtpDatabase {
+class DroidianMtpDatabase : public android::MtpDatabase {
 private:
     struct DbEntry
     {
@@ -236,7 +236,7 @@ private:
     void read_more_notify()
     {
         stream_desc.async_read_some(buf.prepare(buf.max_size()),
-                                    boost::bind(&UbuntuMtpDatabase::inotify_handler,
+                                    boost::bind(&DroidianMtpDatabase::inotify_handler,
                                                 this,
                                                 asio::placeholders::error,
                                                 asio::placeholders::bytes_transferred));
@@ -334,7 +334,7 @@ private:
     }
 
 public:
-    UbuntuMtpDatabase():
+    DroidianMtpDatabase():
         counter(1),
         stream_desc(io_svc),
         work(io_svc),
@@ -351,13 +351,13 @@ public:
 
 	db = std::map<MtpObjectHandle, DbEntry>();
 
-        notifier_thread = boost::thread(&UbuntuMtpDatabase::read_more_notify,
+        notifier_thread = boost::thread(&DroidianMtpDatabase::read_more_notify,
                                        this);
 
         io_service_thread = boost::thread(boost::bind(&asio::io_service::run, &io_svc));
     }
 
-    virtual ~UbuntuMtpDatabase() {
+    virtual ~DroidianMtpDatabase() {
         io_svc.stop();
         notifier_thread.detach();
         io_service_thread.join();
@@ -972,7 +972,7 @@ public:
             info.mName = ::strdup(db.at(handle).display_name.c_str());
             info.mDateCreated = 0;
             info.mDateModified = db.at(handle).last_modified;
-            info.mKeywords = ::strdup("ubuntu,touch");
+            info.mKeywords = ::strdup("droidian");
 
             if (VLOG_IS_ON(2))
                 info.print();
